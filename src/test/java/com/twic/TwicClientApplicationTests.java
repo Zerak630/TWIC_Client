e2bc9464c9c5b25e59d5bf0ea016f30965c17178
Zerak630 <zerak.630@outlook.fr>
2022-12-14 08:43:09 +0100

    Ajout gitignore

---
+++ src/test/java/com/twic/TwicClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwicClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
